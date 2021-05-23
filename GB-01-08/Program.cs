a6821ccc4e1a3b18835e4bfd70bc7b14a50daffd
Artem <a.khimin@yandex.ru>
2021-05-23 15:37:09 +0300

    Приложение с предустановленными параметрами

--- GB-01-08/Program.cs
+++ GB-01-08/Program.cs
@@ -1,13 +1,91 @@
 ﻿using System;
+using System.Configuration;
 
 namespace GB_01_08
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            
+            ReadAllSettings();
 
+            Console.WriteLine("Имя: ");
+            AddUpdateAppSettings("Имя: ", Console.ReadLine());
+            Console.WriteLine("Возраст: ");
+            AddUpdateAppSettings("Возраст: ", Console.ReadLine());
+            Console.WriteLine("Род деятельности: ");
+            AddUpdateAppSettings("Род деятельности: ", Console.ReadLine());
+
+
+            ReadSetting("Имя: ");
+            ReadSetting("Возраст: ");
+            ReadSetting("Род деятельности: ");
+        }
+
+        static void ReadAllSettings()
+        {
+            try
+            {
+                var appSettings = ConfigurationManager.AppSettings;
+
+                Console.WriteLine("Здравствуйте, уважаемый пользователь! ");
+
+                if (appSettings.Count == 0)
+                {
+                    Console.WriteLine("Залолните, пожалуйста, карточку пользователя: ");
+                }
+                else
+                {
+                    appSettings.GetValues(0);
+
+                    foreach (var key in appSettings.AllKeys)
+                    {
+                        Console.WriteLine($"{key} {appSettings[key]}");
+                    }
+                }
+            }
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("Error reading app settings");
+            }
+        }
+
+        static void ReadSetting(string key)
+        {
+            try
+            {
+                var appSettings = ConfigurationManager.AppSettings;
+                string result = appSettings[key] ?? "Not Found";
+                Console.WriteLine(result);
+            }
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("Error reading app settings");
+            }
+        }
+
+        static void AddUpdateAppSettings(string key, string value)
+        {
+            try
+            {
+                var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                var settings = configFile.AppSettings.Settings;
+                if (settings[key] == null)
+                {
+                    settings.Add(key, value);
+                }
+                else
+                {
+                    settings[key].Value = value;
+                }
+                configFile.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+            }
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("Error writing app settings");
+            }
         }
     }
 }
